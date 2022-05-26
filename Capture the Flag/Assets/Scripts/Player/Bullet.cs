837e8b1e8cdd7abd89f513f2dc91eae6ab37156e
Mario <mario_scipio@hotmail.es>
2022-05-26 13:34:05 +0200

    Disparo
    O jogador agora pode atirar como o inferno e causar dano a outros jogadores e não se machucar com suas balas mas parar outras balas com suas balas uma coisa maravilhosa, compadre

---
+++ Capture the Flag/Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.Netcode;
+using System;
+
+[RequireComponent(typeof(NetworkObject))]
+[RequireComponent(typeof(Rigidbody2D))]
+class Bullet : NetworkBehaviour
+    {
+
+    public ulong playerOwner;
+    // Si el servidor detecta una colisión de la bala, si choca con un jugador le quita vida.
+    // En cualquier caso la bala desaparece despues de chocar con algo.
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsServer)
+        {
+            Player target = collision.GetComponent<Player>();
+            // si choca con un muro desaparece
+            if (target == null)
+            {
+                GetComponent<NetworkObject>().Despawn();
+            }
+            // hace daño si choca con un jugador que no sea el que ha disparado y desaparece
+            if (target.OwnerClientId != playerOwner)
+            {
+                target.playerHealth.Value--;
+                GetComponent<NetworkObject>().Despawn();
+            }
+        }
+    }
+}
+
--- Capture the Flag/Assets/Scripts/Player/PlayerController.cs
+++ Capture the Flag/Assets/Scripts/Player/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Unity.Netcode;
+using System;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(CapsuleCollider2D))]
@@ -11,8 +12,11 @@ public class PlayerController : NetworkBehaviour
 {
 
     #region Variables
+    // prefab de la bala que se dispara
+    [SerializeField] Bullet bulletPrefab;
 
     readonly float speed = 3.4f;
+    readonly float bulletSpeed = 2.5f; // velocidad de la bala
     readonly float jumpHeight = 6.5f;
     readonly float gravity = 1.5f;
     readonly int maxJumps = 2;
@@ -53,6 +57,8 @@ public class PlayerController : NetworkBehaviour
         handler.OnMove.AddListener(UpdatePlayerVisualsServerRpc);
         handler.OnJump.AddListener(PerformJumpServerRpc);
         handler.OnMoveFixedUpdate.AddListener(UpdatePlayerPositionServerRpc);
+        // se vincula la función de disparo al botón de disparo
+        handler.OnFire.AddListener(Shoot);
 
         FlipSprite.OnValueChanged += OnFlipSpriteValueChanged;
     }
@@ -62,6 +68,7 @@ public class PlayerController : NetworkBehaviour
         handler.OnMove.RemoveListener(UpdatePlayerVisualsServerRpc);
         handler.OnJump.RemoveListener(PerformJumpServerRpc);
         handler.OnMoveFixedUpdate.RemoveListener(UpdatePlayerPositionServerRpc);
+        handler.OnFire.RemoveListener(Shoot);
 
         FlipSprite.OnValueChanged -= OnFlipSpriteValueChanged;
     }
@@ -145,6 +152,23 @@ public class PlayerController : NetworkBehaviour
         {
             rb.velocity = new Vector2(input.x * speed, rb.velocity.y);
         }
+
+    }
+
+    // el servidor genera una bala y la envía con su velocidad en la dirección a la que apunta la diana
+    [ServerRpc]
+    void PlayerShootServerRpc(Vector3 dir)
+    {
+        
+        
+
+        Vector2 spawnPos = player.transform.position + dir * 0.2f;
+
+        Bullet bullet = Instantiate(bulletPrefab, spawnPos, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D>().velocity = dir * bulletSpeed;
+        bullet.playerOwner = player.OwnerClientId; // guardamos la id del dueño de la bala
+
+        bullet.GetComponent<NetworkObject>().Spawn(true);
     }
 
     #endregion
@@ -169,6 +193,17 @@ public class PlayerController : NetworkBehaviour
     {
         spriteRenderer.flipX = current;
     }
+
+    void Shoot()
+    {
+        // la dirección se calcula antes para evitar que se use la posición del servidor
+        Vector3 dir = player.transform.GetChild(0).position - player.transform.position;
+        dir.Normalize();
+        // pide al servidor que ejecute el disparo
+        PlayerShootServerRpc(dir);
+    }
+
+
 
     // comprobamos la aceleración en el eje y para evitar falsos positivos
     bool IsGrounded => collider.IsTouching(filter) && rb.velocity.y == 0;
